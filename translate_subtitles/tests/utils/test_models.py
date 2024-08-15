3a073c4a9db849d045d38038477b3f3e8299f898
Andrey Pudov <mail@andreypudov.com>
2024-08-15 15:57:38 +0200

    Added a test suite for the models module

---
+++ translate_subtitles/tests/utils/test_models.py
@@ -0,0 +1,29 @@
+"""Test the models module"""
+
+import unittest
+
+from translate_subtitles.utils.models import TRANSLATION_MODEL
+
+
+class TestModels(unittest.TestCase):
+    """
+    Unit test suite for the models module.
+    """
+
+    def test_model_name(self):
+        """
+        Test case to verify the correctness of the model name.
+        """
+        self.assertEqual(
+            "gpt-3.5-turbo-1106",
+            TRANSLATION_MODEL.get("name"),
+            "The model name is incorrect.",
+        )
+
+    def test_model_tokens(self):
+        """
+        Test the number of tokens in the translation model.
+        """
+        self.assertEqual(
+            4_096, TRANSLATION_MODEL.get("tokens"), "The model tokens are incorrect."
+        )
